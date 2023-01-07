1612e2339967528540b584510f5925fb409ffbde
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-07 12:04:47 -0800

    Added basic styling

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const URL = "https://api.thecatapi.com/v1/images/search";
-const KEY = "live_BvivPUxBGDhdkvo0Xk0P5Ji4jjoLSQtqmeUjE3UePkpKpg0y8d7c3CdTN5Ae2te2"
+const KEY =
+  "live_BvivPUxBGDhdkvo0Xk0P5Ji4jjoLSQtqmeUjE3UePkpKpg0y8d7c3CdTN5Ae2te2";
 
 function selectComponent(elementId) {
-    let component = document.getElementById(elementId);
-    return component;
-  }
+  let component = document.getElementById(elementId);
+  return component;
+}
 
-selectComponent("fact-btn").addEventListener("click", function () {
+selectComponent("breed-btn").addEventListener("click", function () {
   fetch(`${URL}?has_breeds=1&api_key=${KEY}`)
     .then((response) => response.json())
     .then((data) => {
-      selectComponent('fact-txt').textContent = (data[0].breeds[0].description);
-      selectComponent('fact-img').innerHTML = `<img src=${data[0].url} />`
+      selectComponent("breed-name").textContent = `The ${data[0].breeds[0].name}`;
+      selectComponent("breed-txt").textContent = data[0].breeds[0].description;
+      selectComponent("breed-img").innerHTML = `<img class="breed-img" src=${data[0].url} />`;
     });
 });
